c58222f7210e69de2346e2bb288218a7861a07e9
Keith Omware <keithomware@gmail.com>
2019-11-29 09:17:26 +0300

    Test getters and setters in UserTest class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,12 @@
 package models;
 
 public class News {
+    private int id;
+    private String content;
+    private String description;
+
+    public News(String content, String description) {
+        this.content = content;
+        this.description = "department news";
+    }
 }
