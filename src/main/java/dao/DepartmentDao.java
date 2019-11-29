852defa2c46cd5d9872a21f7e9f65a8b5d52f180
Keith Omware <keithomware@gmail.com>
2019-11-29 13:35:57 +0300

    Add methods in DepartmentDao, NewsDao and UserDao interfaces

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,24 @@
 package dao;
+import models.News;
+import models.Department;
+import models.User;
+
+import java.util.List;
 
 public interface DepartmentDao {
+
+    // LIST
+    List<Department> getAll();
+    List<User> getUsers(int departmentid);
+    List<News> getNews(int departmentid);
+
+    //create
+    void add (Department department);
+
+    //read
+    Department findById(int id);
+
+   // Delete
+    void deleteById(int id);
+    void clearAll();
 }
--- src/main/java/dao/sql2oDepartmentDao.java
+++ src/main/java/dao/sql2oDepartmentDao.java
@@ -1,4 +1,11 @@
 package dao;
+import models.Department;
+import models.News;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+import java.util.List;
 
 public class sql2oDepartmentDao {
 }
