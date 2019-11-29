e82157b276847d96776bb564022973ea946549fb
Keith Omware <keithomware@gmail.com>
2019-11-29 11:55:44 +0300

    Test getters and setters in NewsTest class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -2,11 +2,43 @@ package models;
 
 public class News {
     private int id;
+    private int departmentid;
     private String content;
     private String description;
 
-    public News(String content, String description) {
+
+    public News(String content, String description, int departmentid) {
         this.content = content;
-        this.description = "department news";
+        this.description = description;
+        this.departmentid = departmentid;
+    }
+    public String getContent() {
+        return content;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getDepartmentid() {
+        return departmentid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDepartmentid(int departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
