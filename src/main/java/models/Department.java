893ae1ab6d52d38f4e731d18917ea62f228ad305
Keith Omware <keithomware@gmail.com>
2019-11-29 08:49:38 +0300

    Test getters and setters in DepartmentTest class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,50 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+    
+        private int id;
+        private String departmentName;
+        private String departmentDescription;
+        private int numberOfEmployees;
+
+        public Department(String departmentName, String description, int numberOfEmployees) {
+            this.departmentName = departmentName;
+            this.departmentDescription = description;
+            this.numberOfEmployees = numberOfEmployees;
+        }
+        public int getId() {
+        return id;
+    }
+        public String getDepartmentName() {
+            return departmentName;
+        }
+        public String getdepartmentDescription() {
+            return departmentDescription;
+        }
+        public int getNumberOfEmployees() {
+            return numberOfEmployees;
+        }
+
+        public void setId(int id) {
+        this.id = id;
+    }
+        public void setDepartmentName(String departmentName) { this.departmentName = departmentName; }
+        public void setDepartmentDescription(String departmentDescription) { this.departmentDescription = departmentDescription; }
+        public void setNumberOfEmployees(int numberOfEmployees) { this.numberOfEmployees = numberOfEmployees; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return numberOfEmployees == that.numberOfEmployees &&
+                Objects.equals(departmentName, that.departmentName) &&
+                Objects.equals(departmentDescription, that.departmentDescription);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentName, departmentDescription, numberOfEmployees);
+    }
 }
--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,4 +1,51 @@
 package models;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class DepartmentTest {
+    @Test
+    public void departmentInstanciatesCorrectly() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        assertEquals (true, department instanceof Department);
+    }
+
+    @Test
+    public void getsDepartmentName() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        assertEquals ("Accounting", department.getDepartmentName());
+    }
+    @Test
+    public void getsDepartmentDescription() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        assertEquals ("Deals with Accounting", department.getdepartmentDescription());
+    }
+    @Test
+    public void getsDepartmentEmployees() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        assertEquals (50, department.getNumberOfEmployees());
+    }
+    @Test
+    public void setsDepartmentName() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        department.setDepartmentName("Accounting");
+        assertEquals("Accounting", department.getDepartmentName());
+    }
+    @Test
+    public void setsDepartmentDescription() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        department.setDepartmentDescription("Deals with Accounting");
+        assertEquals("Deals with Accounting", department.getdepartmentDescription());
+    }
+    @Test
+    public void setsDepartmentEmployees() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        department.setNumberOfEmployees(50);
+        assertEquals(50, department.getNumberOfEmployees());
+    }
+    @Test
+    public void setId() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        department.setId(5);
+        assertEquals(5, department.getId());
+    }
 }
