8d5b65d7d5000219203139863635d9fd068b709e
Keith Omware <keithomware@gmail.com>
2019-12-03 07:39:33 +0300

    Add process builder in app.java class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,6 +14,16 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
         sql2oUserDao UserDao;
         sql2oDepartmentDao DepartmentDao;
         sql2oNewsDao NewsDao;
